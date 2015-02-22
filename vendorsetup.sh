f9d2fc7a732236d63d2bcd0f01f4b5a82ed30a77
Artur <slfl@mail.ru>
2015-02-22 13:45:30 +0300

    new updates

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 export OTA_DEVELOPER_ID=mansi
 
-if [[ "$THETA_PATCH_SOURCE" -eq 1 ]]
+if [[ "$MANSI_PATCH_SOURCE" -eq 1 ]]
 then
   echo ""
   sh vendor/mansi/merge/merge.sh
   echo ""
-  export THETA_PATCH_SOURCE=0
+  export MANSI_PATCH_SOURCE=0
 fi
