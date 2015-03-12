d05679753a05423a2a80e0c4439984c7779a2c7c
Artur <slfl@mail.ru>
2015-03-12 19:53:28 +0300

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is patches for Huawei Y300/G510
 -------------------------------------------------------------------------------
 
-Credits: Dazzozo, ThetaOrb, Chill360.
+Credits: Dazzozo, ThetaOrb, Chil360.
--- config/common_full.mk
+++ config/common_full.mk
@@ -1,9 +1,9 @@
 # copy prebuilt
 PRODUCT_COPY_FILES += \
     vendor/mansi/prebuilt/etc/hosts:system/etc/hosts \
-    vendor/theta/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/theta/prebuilt/app/RootExplorer/RootExplorer.apk:system/app/RootExplorer/RootExplorer.apk
+    vendor/mansi/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/mansi/prebuilt/app/RootExplorer/RootExplorer.apk:system/app/RootExplorer/RootExplorer.apk
 
 # init.d scripts
 PRODUCT_COPY_FILES += \
-    vendor/theta/prebuilt/etc/init.d/99Swap:system/etc/init.d/99Swap
+    vendor/mansi/prebuilt/etc/init.d/99Swap:system/etc/init.d/99Swap
